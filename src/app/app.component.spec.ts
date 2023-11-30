1de57eec72616551a4fbd937b7b9c7486c1b7d9c
Morgan-sama <morganeangoc@gmail.com>
2023-11-30 12:32:13 +0100

    Initialisation du projet CICD avec pipeline d'intégration et de deploiement continus

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,6 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+
+
---
+++ src/app/page/page.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageComponent } from './page.component';
+
+describe('PageComponent', () => {
+  let component: PageComponent;
+  let fixture: ComponentFixture<PageComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PageComponent]
+    });
+    fixture = TestBed.createComponent(PageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have as text 'Morgane Claudel ANGO-ING4 ISI'`,()=>{
+    const fixture = TestBed.createComponent(PageComponent)
+    const accueil = fixture.componentInstance;
+    expect(accueil.infos).toEqual('Morgane Claudel ANGO-ING4 ISI');
+  })
+
+  
+});
